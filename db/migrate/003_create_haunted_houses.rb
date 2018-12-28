8e666c25f86e4052377099902f931ef052f3e4b0
Cheng Vue <cvu3@ucsc.edu>
2018-12-28 21:58:26 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
-# Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :haunted_house do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.string :opening_date
+      t.string :closing_date
+    end
+  end
+end
